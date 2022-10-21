ca926db15777b3c9182611e29e2ea9d2ebfbd268
Elena Potapova <115479681+Potapova-Elena@users.noreply.github.com>
2022-10-21 19:06:39 +0300

    Update task1.py
    added year capitalization and while loop

--- Tasks/Potapova/Task1/task1.py
+++ Tasks/Potapova/Task1/task1.py
@@ -5,13 +5,27 @@ deposit = dec(input('Enter the deposit amount please\n'))
 percent = dec(input('Enter percentage per annum please\n'))
 term_year = int(input('Enter number of years please\n'))
 term_month = term_year*12                                      # months used in calculations
+capit = ''
 
-if input('Whether to take into account the capitalization of savings per month? (y/n)\n').lower() == 'y':
-    cash = deposit * ((percent / 1200 + 1) ** term_month)
-    cash = round(cash, 2)
-else:
-    cash = deposit * (percent * term_month / 1200 + 1)
-    cash = round(cash, 2)
+while capit != 'y' and capit != 'n':
+    capit = input('Whether to take into account the capitalization of savings?\n (y/n)\n').lower()
+    if capit.lower() == 'y':
+        capit_term = ''
+        while capit_term != 'y' and capit_term != 'm':
+            capit_term = input('Should capitalization be per year or per month?\n (y/m)\n').lower()
+            if capit_term.lower() == 'm':
+                cash = deposit * ((percent / 1200 + 1) ** term_month)
+                cash = round(cash, 2)
+            elif capit_term.lower() == 'y':
+                cash = deposit * ((percent / 100 + 1) ** term_year)
+                cash = round(cash, 2)
+            else:
+                print('Please, make a choice!')
+    elif capit.lower() == 'n':
+        cash = deposit * (percent * term_month / 1200 + 1)
+        cash = round(cash, 2)
+    else:
+        print ('Please, make a choice!')
 
 
 print(f'Your final sum will be {cash},\nprofit is {cash - deposit}\n')
