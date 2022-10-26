7ee141f8c7e6c315831a66fc09573458d501e4c1
DmitreyNEW <112245524+NovikauDmitrey@users.noreply.github.com>
2022-10-26 14:14:52 +0300

    Finished-HW1

--- Tasks/Novikau_Dmitrey/task1/task-1.py
+++ Tasks/Novikau_Dmitrey/task1/task-1.py
@@ -1,8 +1,16 @@
 import math
-o = float(input("Enter o:\n"))
-p = float(input("Enter p:\n"))
-n = float(input("Enter n:\n"))
+capitalization = input("Include monthly capitalization (enter yes/no):\n")
+o = float(input("Enter start-up money:\n"))
+p = float(input("Enter percent:\n"))
+n = int(input("Enter term (in years):\n"))
 
-S = int(o*(1+1/100*p)**n)
 
-print(S)
+F = int(o*(1+n*p/100))
+S = int(o*(1+p/100/12)**(n*12))
+
+if capitalization == "yes":
+    print("Your amount for" , n , "years:\n" , S)
+elif capitalization == "no":
+    print("Your amount for" , n , "years:\n" , F)
+else:
+    print("Sorry!")
