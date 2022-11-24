864fb09210f1c8edca594159eca2d7321d571915
Elena Potapova <115479681+Potapova-Elena@users.noreply.github.com>
2022-11-24 17:57:53 +0300

    Update task5.py
    made the second task work faster

--- Tasks/Potapova/Task5/task5.py
+++ Tasks/Potapova/Task5/task5.py
@@ -1,7 +1,7 @@
 # TASK 1
 import re
 
-def check_str(s: str):
+def check_str(s: str) -> str:
     s = re.sub('[^A-Za-zА-Яа-я]+', '', s)
     s1 = re.sub('[A-ZА-Я]+', '', s)
     return f'{len(s)-len(s1)} upper case, {len(s1)} lower case'
@@ -11,10 +11,13 @@ print(check_str('The quick Brown Fox'))
 
 
 # TASK 2
-def is_prime(num: int):
+def is_prime(num: int) -> bool:
     if num == 1:
         return False
-    return True not in [num % i == 0 for i in range(2, round(num**(0.5)+1))]
+    for i in range(2, round(num**(0.5)+1)):
+        if num % i == 0:
+            return False
+    return True
 
 
 print(is_prime(787))
@@ -22,7 +25,7 @@ print(is_prime(777))
 
 
 # TASK 3
-def get_ranges(l: list):
+def get_ranges(l: list) -> str:
     new_l = str(l[0])
     for pos in range(len(l)-1):
         if l[pos + 1] != l[pos] + 1 and str(l[pos]) == new_l[-1]:
