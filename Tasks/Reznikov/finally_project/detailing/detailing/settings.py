671fbd3194ceb6d2c9100c8416dd090dec0dc356
JamesDiablo <74411318+JamesDiablo@users.noreply.github.com>
2023-01-14 18:03:46 +0300

    Update settings.py
    update debug = true

--- Tasks/Reznikov/finally_project/detailing/detailing/settings.py
+++ Tasks/Reznikov/finally_project/detailing/detailing/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=vu(&-(00=zrs-tehk*2sl7g5v3vy3ue-tk0dm@$^bx_1e3y9&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
