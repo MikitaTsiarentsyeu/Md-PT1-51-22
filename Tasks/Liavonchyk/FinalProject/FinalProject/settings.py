9723f0ac573098aea32dfa76bdf7ca1b78b489c8
Aleksandr Liavonchyk <aleksandr.liavonchyk@p9dt.com>
2023-01-17 11:49:50 +0300

    clean key

--- Tasks/Liavonchyk/FinalProject/FinalProject/settings.py
+++ Tasks/Liavonchyk/FinalProject/FinalProject/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a6#ukv1_(+gfaxzg8vo8jfy6=posq3@6k-21q3cm%0^*r%+!in'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
